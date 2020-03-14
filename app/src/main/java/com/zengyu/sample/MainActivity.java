a492ef3f8680b32db10a202b878ce86471ff3060
zengyu.zhan <zengyu.zhan@dji.com>
2020-03-14 16:43:10 +0800

    use findViewById to get component

--- app/src/main/java/com/zengyu/sample/MainActivity.java
+++ app/src/main/java/com/zengyu/sample/MainActivity.java
@@ -3,6 +3,7 @@ package com.zengyu.sample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView tvCenter = findViewById(R.id.tv_center);
+        tvCenter.setText("Text Change");
     }
 }
